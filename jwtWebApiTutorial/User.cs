78c08db6bb88cd171dfeccb3eb9d4ea85bfcc9c3
Aymen Mabrouk <amabrouk@edligo.com>
2022-05-30 15:07:07 +0100

    add refrech Token

--- jwtWebApiTutorial/User.cs
+++ jwtWebApiTutorial/User.cs
@@ -5,8 +5,9 @@
         public string UserName { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-
-                                                
+        public string RefrechToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
 
     }
 }
